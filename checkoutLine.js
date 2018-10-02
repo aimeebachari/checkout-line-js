df8df53c01152e5cfcc440faf297750175fa2a6c
Aimee Bachari <aimeebachari@gmail.com>
2018-10-02 08:46:02 -0500

    second example

--- checkoutLine.js
+++ checkoutLine.js
@@ -28,5 +28,14 @@ let coffeeItem = shoppingCart.find((item) => item.itemName === `lbs. of coffee`)
 
 let milkItem = shoppingCart.find((item) => item.itemName === `gallon of milk`);
 
-console.log(`One pound of coffee is $${coffeeItem.price}.`)
-console.log(`There are ${milkItem.quantity} gallons of milk in the cart.`)
+console.log(`One pound of coffee is $${coffeeItem.price}.`);
+console.log(`There are ${milkItem.quantity} gallons of milk in the cart.`);
+
+let total = 0;
+const TAX_RATE = 6.25;
+shoppingCart.forEach((item) => {
+  total = total + item.price * item.quantity;
+});
+
+console.log(`SUBTOTAL: ${total}`);
+console.log(`TOTAL:${total * (1 + (TAX_RATE / 100))}`);
