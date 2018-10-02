7b933408e568d4a454cc8066824e924683fa9bb6
Aimee Bachari <aimeebachari@gmail.com>
2018-10-02 08:36:37 -0500

    first example

--- checkoutLine.js
+++ checkoutLine.js
@@ -2,26 +2,31 @@ let eggs = {
   itemName: 'dozen eggs',
   quantity: 2,
   price: 0.99
-}
+};
 
 let milk = {
   itemName: 'gallon of milk',
   quantity: 1,
   price: 2.99
-}
+};
 
 let bread = {
   itemName: 'loaf of bread',
   quantity: 1,
   price: 3.50
-}
+};
 
 let coffee = {
   itemName: 'lbs. of coffee',
   quantity: 10,
   price: 8.99
-}
+};
 
-let shoppingCart = [eggs, milk, bread, coffee]
+let shoppingCart = [eggs, milk, bread, coffee];
 
-// your code, here
+let coffeeItem = shoppingCart.find((item) => item.itemName === `lbs. of coffee`);
+
+let milkItem = shoppingCart.find((item) => item.itemName === `gallon of milk`);
+
+console.log(`One pound of coffee is $${coffeeItem.price}.`)
+console.log(`There are ${milkItem.quantity} gallons of milk in the cart.`)
